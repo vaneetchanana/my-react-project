2bac5d7664da2fb3d52da814813fbe6de273a642
Vaneet Chanana <vaneetchanana98@gmail.com>
2023-06-25 13:56:55 +0530

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,48 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-function App() {
+
+function Home() {
   const [count, setCount] = useState(0)
-
   return (
     <>
-    <div>{count}</div>
-    <button onClick={() => setCount(count + 1) }>Increase Count</button>
+      <h1>Home-page</h1>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+    </>
+  )
+}
+
+
+function About() {
+  return (
+    <>
+      <h1>About-page</h1>
+    </>
+  )
+}
+
+
+function Contact() {
+  return (
+    <>
+      <h1>Contact-page</h1>
+    </>
+  )
+}
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
